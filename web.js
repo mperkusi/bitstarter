ed36a72e0329a4eb11f4825461efb6eaf1a5b18b
Mirko Perkusich <mirkooooo@gmail.com>
2013-07-09 15:15:30 +0000

    Fixed problem in web.js

--- web.js
+++ web.js
@@ -1,15 +1,14 @@
 var express = require('express');
+var fs = require('fs'); 
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile('index.html', function(err, data){
+ fs.readFile('index.html', function(err, data){
       if (err) throw err;
-      var buffer = new Buffer(16);
-      buffer.write(data, "utf-8");
-      response.send(data.toString());
+      var buf = new Buffer(data);
+      response.send(buf.toString());
   });
-  // response.send('Hello world 2!');     
 });
 
 var port = process.env.PORT || 5000;
